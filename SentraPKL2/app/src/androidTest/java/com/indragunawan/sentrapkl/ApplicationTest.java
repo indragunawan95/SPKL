383d3589de0024afb47667410eaee03139f24954
I Putu Gede Indra Gunawan <i.putu.indra13@mhs.if.its.ac.id>
2016-09-26 21:37:26 +0800

    first commit

---
+++ SentraPKL2/app/src/androidTest/java/com/indragunawan/sentrapkl/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.indragunawan.sentrapkl;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
